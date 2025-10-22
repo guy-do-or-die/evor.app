35478e976c2399c11c44bf69f6df40f93f1499e8
Egor Gusev <guy.do.or.die@gmail.com>
2025-10-23 02:51:17 +0400

    feat: initialize vite project with react and typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import evorLogo from './assets/evor.svg'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://evor.app" target="_blank">
+          <img src={evorLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>evor.app</h1>
+    </>
+  )
+}
+
+export default App
